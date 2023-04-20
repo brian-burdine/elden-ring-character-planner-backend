701ee48d5a22082c09118760d3604bcb6311115e
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-04-20 22:11:30 +0000

    Started adding seeding for non-API tables

--- ercharacterplanner/managment/commands/seed.py
+++ ercharacterplanner/managment/commands/seed.py
@@ -1,9 +1,23 @@
 import requests
 from django.core.management.base import BaseCommand
-from models import Armament, Armor, Ash_Of_War, Great_Rune, Spell, Talisman
+from ...models import (
+    Armament, 
+    Armor, 
+    Ash_Of_War, 
+    Great_Rune, 
+    Spell, 
+    Talisman,
+    Main_Attribute,
+    Starting_Class,
+    Starting_Class_Attribute
+)
 
 # This module defines a custom CLI command to grab information from a data API
 #   and store it in the database. 
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    pass
 
 # API URL to get data from latest game version
 BASE_URL = "https://api.erdb.wiki/v1/latest/"
@@ -108,6 +122,139 @@ def seed_talismans():
             name=tali,
             data=talismans[tali]
         )
+
+# Personally-defined Tables
+# These are seed functions for tables that I didn't find a useful API to 
+#   populate with. Modifications seem unlikely to Main_Attribute, 
+#   Starting_Class, and Starting_Class_Attribute, but there probably will be
+#   new Skills added in Shadow of the Erdtree. Hopefully I can find a useful
+#   source for those
+
+def seed_main_attributes():
+    attributes = (
+        "Vigor", "Mind", "Endurance", "Strength", "Dexterity", "Intelligence",
+        "Faith", "Arcane"
+    )
+    for attribute in attributes:
+        att = Main_Attribute(
+            name=attribute
+        )
+        att.save()
+
+def seed_starting_classes():
+    starting_classes = (
+        "Vagabond", "Warrior", "Hero", "Bandit", "Astrologer", "Prophet", 
+        "Samurai", "Prisoner", "Confessor", "Wretch"
+    )
+    for start_class in starting_classes:
+        sc = Starting_Class(
+            name=start_class
+        )
+        sc.save()
+
+def seed_starting_class_attributes():
+    starting_class_attributes = {
+        "Vagabond": {
+            "Vigor": 15,
+            "Mind": 10,
+            "Endurance": 11,
+            "Strength": 14,
+            "Dexterity": 13,
+            "Intelligence": 9,
+            "Faith": 9,
+            "Arcane": 7
+        },
+        "Warrior": {
+            "Vigor": 11,
+            "Mind": 12,
+            "Endurance": 11,
+            "Strength": 10,
+            "Dexterity": 16,
+            "Intelligence": 10,
+            "Faith": 8,
+            "Arcane": 9
+        },
+        "Hero": {
+            "Vigor": 14,
+            "Mind": 9,
+            "Endurance": 12,
+            "Strength": 16,
+            "Dexterity": 9,
+            "Intelligence": 7,
+            "Faith": 8,
+            "Arcane": 11
+        },
+        "Bandit": {
+            "Vigor": 10,
+            "Mind": 11,
+            "Endurance": 10,
+            "Strength": 9,
+            "Dexterity": 13,
+            "Intelligence": 9,
+            "Faith": 8,
+            "Arcane": 14
+        },
+        "Astrologer": {
+            "Vigor": 9,
+            "Mind": 15,
+            "Endurance": 9,
+            "Strength": 8,
+            "Dexterity": 12,
+            "Intelligence": 16,
+            "Faith": 7,
+            "Arcane": 9
+        },
+        "Prophet": {
+            "Vigor": 10,
+            "Mind": 14,
+            "Endurance": 8,
+            "Strength": 11,
+            "Dexterity": 10,
+            "Intelligence": 7,
+            "Faith": 16,
+            "Arcane": 10
+        },
+        "Samurai": {
+            "Vigor": 12,
+            "Mind": 11,
+            "Endurance": 13,
+            "Strength": 12,
+            "Dexterity": 15,
+            "Intelligence": 9,
+            "Faith": 8,
+            "Arcane": 8
+        },
+        "Prisoner": {
+            "Vigor": 11,
+            "Mind": 12,
+            "Endurance": 11,
+            "Strength": 11,
+            "Dexterity": 14,
+            "Intelligence": 14,
+            "Faith": 6,
+            "Arcane": 9
+        },
+        "Confessor": {
+            "Vigor": 10,
+            "Mind": 13,
+            "Endurance": 10,
+            "Strength": 12,
+            "Dexterity": 12,
+            "Intelligence": 9,
+            "Faith": 14,
+            "Arcane": 9
+        },
+        "Wretch": {
+            "Vigor": 10,
+            "Mind": 10,
+            "Endurance": 10,
+            "Strength": 10,
+            "Dexterity": 10,
+            "Intelligence": 10,
+            "Faith": 10,
+            "Arcane": 10
+        }
+    }
 
 # # API Test
 # great_runes = get_great_runes()
--- ercharacterplanner/models.py
+++ ercharacterplanner/models.py
@@ -8,8 +8,8 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-## Data Models
-## Grabbed from ERDB API (https://api.erdb.wiki/v1/latest/)
+# DATA MODELS
+#1. Grabbed from ERDB API (https://api.erdb.wiki/v1/latest/)
 
 class Armament (models.Model):
     name = models.CharField(max_length=75)
@@ -58,6 +58,26 @@ class Talisman (models.Model):
 
     def __str__(self):
         return self.name
+
+#2. Self-defined
+
+class Main_Attribute (models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Starting_Class (models.Model):
+    name = models.CharField(max_length=20)
+    attributes = models.ManyToManyField(Main_Attribute, through='Starting_Class_Attribute')
+
+    def __str__(self):
+        return self.name
+
+class Starting_Class_Attribute (models.Model):
+    starting_class = models.ForeignKey(Starting_Class, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Main_Attribute(), on_delete=models.CASCADE)
+    base_value = models.IntegerField()
 
 # class Weapon (models.Model):
 #     name = models.CharField(max_length=75)
